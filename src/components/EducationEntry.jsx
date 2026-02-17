d0b7c0512669b953ca1554cfc63be4bf3dc6f6b2
Vangala Pranitej <137621721+Pranitej@users.noreply.github.com>
2026-02-17 15:58:03 +0530

    change gpa

--- src/components/EducationEntry.jsx
+++ src/components/EducationEntry.jsx
@@ -15,7 +15,7 @@ const EducationEntry = () => {
       <div className="flex flex-col sm:flex-row sm:justify-between">
         <p>Computer Science Engineering (AI and ML)</p>
         <p className="text-sm text-gray-600 dark:text-gray-300 sm:text-right sm:mt-0 mt-1">
-          GPA – 7.3/10
+          GPA – 7.4/10
         </p>
       </div>
     </div>
@@ -23,3 +23,4 @@ const EducationEntry = () => {
 };
 
 export default EducationEntry;
+
